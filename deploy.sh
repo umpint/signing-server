a45351c318a61e320c4da91b061b816af8702310
umpint <robin.owens@umpint.com>
2020-08-04 13:27:06 +0100

    update before beta release

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # save your account id into the acountid file - it is a 12 digit number
 # you also need to have config and credetials set up and aws tools installed.
-# The account in the credentials needs write access to AWS ECR and a repository created called signing-server
+# The account in the credentials needs write access to AWS ECR and a repository created called umpint-web
 AWS_ACCOUNT_ID=`cat ~/.aws/accountid`
 AWS_REGION=`cat ~/.aws/config | grep region | awk '{print $3}'| head -1`
 echo "account $AWS_ACCOUNT_ID"
@@ -10,10 +10,17 @@ tag=$1
 
 if [ "_$tag" == "_" ];then
 	tag=latest
+	git tag -f $tag
+else
+	git tag $tag
 fi
+
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 docker build -t signing-server .
+if [ $tag != latest ];then
+	docker tag signing-server:latest signing-server:$tag
+fi
 docker tag signing-server:$tag ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/signing-server:$tag
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/signing-server:$tag
